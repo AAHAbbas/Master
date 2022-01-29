bce2c9c49721ac693a15f8f458c540d300d02107
Ahmed Abbas <ahmed.a.hussein.a@gmail.com>
2022-01-29 21:49:54 +0100

    ES Service add field to an index

--- FacetedSearchIndex/search/src/main/java/com/search/utils/ESService.java
+++ FacetedSearchIndex/search/src/main/java/com/search/utils/ESService.java
@@ -2,7 +2,9 @@ package com.search.utils;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import com.fasterxml.jackson.databind.introspect.DefaultAccessorNamingStrategy.FirstCharBasedValidator;
 import com.search.repository.ElasticSearchRepository;
 import com.search.types.Field;
 
@@ -19,6 +21,9 @@ import co.elastic.clients.elasticsearch._types.mapping.TypeMapping;
 import co.elastic.clients.elasticsearch._types.mapping.TypeMapping.Builder;
 import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
 import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
+import co.elastic.clients.elasticsearch.indices.PutMappingRequest;
+import co.elastic.clients.elasticsearch.indices.PutMappingResponse;
+import co.elastic.clients.util.ObjectBuilder;
 
 public class ESService {
     private static final Logger LOGGER = LogManager.getLogger(ESManager.class);
@@ -39,34 +44,26 @@ public class ESService {
 
         try {
             CreateIndexResponse response = repo.indexCreate(request);
-            LOGGER.info("configIndex finished : " + response);
-        } catch (IOException e) {
-            LOGGER.error("Caught IOException when trying to create an ES index");
+            LOGGER.info("Successfully created an index: " + response);
+        } catch (ElasticsearchException | IOException e) {
+            LOGGER.error("An exception occured when trying to create an ES index");
             e.printStackTrace();
         }
     }
 
-    private TypeMapping createMapping(ArrayList<Field> fields) {
-        Builder builder = new TypeMapping.Builder();
-        for (Field field : fields) {
-            co.elastic.clients.elasticsearch._types.mapping.Property.Builder property = new Property.Builder();
+    public void addFieldToIndex(String indexName, Field field) {
+        ObjectBuilder<Property> property = getProperty(field.type);
+        PutMappingRequest request = new PutMappingRequest.Builder()
+                .index(Arrays.asList(indexName))
+                .properties(field.name, property.build()).build();
 
-            if (field.type.equals("text")) {
-                property.text(new TextProperty.Builder().analyzer("standard").build());
-            } else if (field.type.equals("keyword")) {
-                property.keyword(new KeywordProperty.Builder().build());
-            } else if (field.type.equals("long")) {
-                property.long_(new LongNumberProperty.Builder().nullValue(0L).build());
-            } else if (field.type.equals("float")) {
-                property.float_(new FloatNumberProperty.Builder().build());
-            } else {
-                LOGGER.error("Invalid Elasticsearch field type");
-            }
-
-            builder.properties(field.name, property.build());
+        try {
+            PutMappingResponse response = repo.updateMapping(request);
+            LOGGER.info("Successfully updated the mapping of index [" + indexName + "]:" + response);
+        } catch (ElasticsearchException | IOException e) {
+            LOGGER.error("An exception occured when trying to update the mapping to index [" + indexName + "]");
+            e.printStackTrace();
         }
-
-        return builder.build();
     }
 
     // Delete an index by specifying index name
@@ -74,12 +71,42 @@ public class ESService {
         try {
             repo.deleteIndex(indexName);
             LOGGER.info("Successfully deleted index [" + indexName + "]");
-        } catch (IOException | ElasticsearchException e) {
+        } catch (ElasticsearchException | IOException e) {
             if (e.getMessage().contains("index_not_found_exception")) {
                 LOGGER.error("Cannot delete index [" + indexName + "] because it doesn't exists");
             }
 
             e.printStackTrace();
+        }
+    }
+
+    private TypeMapping createMapping(ArrayList<Field> fields) {
+        Builder builder = new TypeMapping.Builder();
+        for (Field field : fields) {
+            ObjectBuilder<Property> property = getProperty(field.type);
+
+            if (property != null) {
+                builder.properties(field.name, property.build());
+            }
+        }
+
+        return builder.build();
+    }
+
+    private ObjectBuilder<Property> getProperty(String type) {
+        co.elastic.clients.elasticsearch._types.mapping.Property.Builder property = new Property.Builder();
+
+        if (type.equals("text")) {
+            return property.text(new TextProperty.Builder().analyzer("standard").build());
+        } else if (type.equals("keyword")) {
+            return property.keyword(new KeywordProperty.Builder().build());
+        } else if (type.equals("long")) {
+            return property.long_(new LongNumberProperty.Builder().nullValue(0L).build());
+        } else if (type.equals("float")) {
+            return property.float_(new FloatNumberProperty.Builder().build());
+        } else {
+            LOGGER.error("Invalid Elasticsearch field type");
+            return null;
         }
     }
 }
