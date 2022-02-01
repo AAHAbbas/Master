e6af3fe9b6ead44a539062ff1464e5cdda2ba376
Ahmed Abbas <ahmed.a.hussein.a@gmail.com>
2022-02-01 05:22:04 +0100

    Add field class

---
+++ FacetedSearchIndex/search/src/main/java/com/search/types/Field.java
@@ -0,0 +1,11 @@
+package com.search.types;
+
+public class Field {
+    public String name;
+    public String type;
+
+    public Field(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+}
