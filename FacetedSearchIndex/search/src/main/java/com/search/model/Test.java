748a428fcc6c4c9ab533237630233815d0fa44f1
Ahmed Abbas <ahmed.a.hussein.a@gmail.com>
2022-02-01 05:40:45 +0100

    testing stuff

---
+++ FacetedSearchIndex/search/src/main/java/com/search/model/Test.java
@@ -0,0 +1,49 @@
+package com.search.model;
+
+public class Test {
+    private String field0;
+    private String field1;
+    private String field2;
+    private String field3;
+    private String field4;
+
+    public String getField0() {
+        return field0;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    public String getField4() {
+        return field4;
+    }
+
+    public void setField0(String value) {
+        field0 = value;
+    }
+
+    public void setField1(String value) {
+        field1 = value;
+    }
+
+    public void setField2(String value) {
+        field2 = value;
+    }
+
+    public void setField3(String value) {
+        field3 = value;
+    }
+
+    public void setField4(String value) {
+        field4 = value;
+    }
+}
