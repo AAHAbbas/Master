fd29177d08c1c42901e320263d989f79e64f6504
Ahmed Abbas <ahmed.a.hussein.a@gmail.com>
2022-02-03 05:43:55 +0100

    Fix usage of deprecated classes

--- FacetedSearchIndex/search/src/main/java/com/search/utils/SparqlQueryVisitor.java
+++ FacetedSearchIndex/search/src/main/java/com/search/utils/SparqlQueryVisitor.java
@@ -12,34 +12,32 @@ import org.eclipse.rdf4j.query.algebra.Compare;
 import org.eclipse.rdf4j.query.algebra.Regex;
 import org.eclipse.rdf4j.query.algebra.StatementPattern;
 import org.eclipse.rdf4j.query.algebra.ValueExpr;
-import org.eclipse.rdf4j.query.algebra.helpers.QueryModelVisitorBase;
+import org.eclipse.rdf4j.query.algebra.helpers.AbstractQueryModelVisitor;
 import org.eclipse.rdf4j.query.parser.ParsedQuery;
 import org.eclipse.rdf4j.query.parser.sparql.SPARQLParser;
-import org.jgrapht.DirectedGraph;
+import org.jgrapht.Graph;
 import org.jgrapht.graph.ClassBasedEdgeFactory;
 import org.jgrapht.graph.DirectedMultigraph;
 
 // Class used to parse a SPARQL query construct all the parts needed to construct e.g an abstract query.
-public class SparqlQueryVisitor extends QueryModelVisitorBase {
-
+public class SparqlQueryVisitor extends AbstractQueryModelVisitor<Exception> {
     public Set<ValueExpr> regexFilters = new HashSet<ValueExpr>();
     public Set<ValueExpr> compareFilters = new HashSet<ValueExpr>();
     public Set<StatementPattern> statementPatterns = new HashSet<StatementPattern>();
 
-    DirectedGraph<Variable, LabeledEdge> directedGraph = new DirectedMultigraph<Variable, LabeledEdge>(
+    Graph<Variable, LabeledEdge> directedGraph = new DirectedMultigraph<Variable, LabeledEdge>(
             new ClassBasedEdgeFactory<Variable, LabeledEdge>(LabeledEdge.class));
     ConceptVariable root;
     Map<Variable, Set<Filter>> filters;
 
     // Constructor. Takes a partial query, parses it and saves it as a variable
     public SparqlQueryVisitor(String SPARQLQuery) throws Exception {
-
         // Make parser and parse query
         SPARQLParser parser = new SPARQLParser();
         ParsedQuery parsedQuery = parser.parseQuery(SPARQLQuery, null);
 
-        // Visit. This stores all the useful information in regexFilters, compareFilters
-        // and statementPatterns.
+        // This stores all the useful information in regexFilters, compareFilters and
+        // statementPatterns.
         parsedQuery.getTupleExpr().visit(this);
     }
 
